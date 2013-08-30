06ff82535dc3425116a4c32f02e83bf616760a0a
Michael JasonSmith <mpj17@onlinegroups.net>
2013-08-30 11:26:49 -0700

    Adding a copyright and license.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -7,41 +7,53 @@ version = get_version()
 
 setup(name='gs.profile.signup.facebook',
     version=version,
-    description="The pages and code required for user initiated sign-up via"\
+    description="The pages and code required for user initiated sign-up via"
         "facebook (registration)",
     long_description=open("README.txt").read() + "\n" +
                     open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='sign up, registration, profile, user, join, facebook',
     author='Richard Waid',
     author_email='richard@onlinegroups.net',
     url='http://groupserver.org',
-    license='ZPL',
+    license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.profile', 'gs.profile.signup'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'setuptools',
-        'Products.GSProfile',
+        'zope.component',
+        'zope.formlib',
+        'zope.interface',
+        'zope.schema',
+        'zope.viewlet',
+        'Zope2',
+        'gs.auth.oauth',
         'gs.content.form',
-        'gs.group.member.join',
+        'gs.group.base',
         'gs.group.member.invite.base',
+        'gs.group.member.join',
+        'gs.option',
+        'gs.profile.email.base',
+        'gs.profile.email.verify',
         'gs.profile.password',
-        # -*- Extra requirements: -*-
+        'gs.profile.signup.base',
+        'Products.CustomUserFolder',
+        'Products.GSProfile',
+        'Products.XWFCore',
     ],
     entry_points="""
     # -*- Entry points: -*-
     """,
 )
-
