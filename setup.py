6fd6688a20741957867a31cfd56c4f161b602126
Michael JasonSmith <mpj17@onlinegroups.net>
2014-06-11 17:26:14 -0700

    Updating the product metadata

--- setup.py
+++ setup.py
@@ -12,18 +12,23 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
+import codecs
 import os
 from setuptools import setup, find_packages
 from version import get_version
 
 version = get_version()
 
+with codecs.open('README.txt', encoding='utf-8') as f:
+    long_description = f.read()
+with codecs.open(os.path.join("docs", "HISTORY.txt"), encoding='utf-8') as f:
+    long_description += '\n' + f.read()
+
 setup(name='gs.profile.signup.facebook',
     version=version,
     description="The pages and code required for user initiated sign-up via"
         "facebook (registration)",
-    long_description=open("README.txt").read() + "\n" +
-                    open(os.path.join("docs", "HISTORY.txt")).read(),
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
@@ -31,14 +36,16 @@ setup(name='gs.profile.signup.facebook',
         "Intended Audience :: Developers",
         'License :: OSI Approved :: Zope Public License',
         "Natural Language :: English",
-        "Operating System :: POSIX :: Linux"
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='sign up, registration, profile, user, join, facebook',
     author='Richard Waid',
     author_email='richard@onlinegroups.net',
-    url='http://groupserver.org',
+    maintainer='Michael JasonSmith',
+    maintainer_email='mpj17@onlinegroups.net',
+    url='https://source.iopen.net/groupserver/gs.profile.signup.facebook/',
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs', 'gs.profile', 'gs.profile.signup'],
@@ -51,10 +58,12 @@ setup(name='gs.profile.signup.facebook',
         'zope.formlib',
         'zope.interface',
         'zope.schema',
+        'zope.tal',
+        'zope.tales',
         'zope.viewlet',
         'Zope2',
         'gs.auth.oauth.client',
-        'gs.content.form',
+        'gs.content.form.base',  # For the status message content provider.
         'gs.content.js.wymeditor[zope]',
         'gs.content.layout',
         'gs.group.base',
